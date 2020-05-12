b9fae1939a015a8dff98108479616e5e885757f5
kamilko250 <freshmultikk21@gmail.com>
2020-05-12 16:02:58 +0200

    1

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -15,9 +15,9 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
---
+++ WpfApp1/WpfApp1/SimplePerson.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    class SimplePerson
+    { 
+    
+    
+    }
+}
