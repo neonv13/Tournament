20c4e2d73e51dd658f9ccbd68cb1b96621a73ee0
kamilko250 <freshmultikk21@gmail.com>
2020-05-23 00:15:15 +0200

    Working on MatchesList class

--- Tournament/Models/MatchList.cs
+++ Tournament/Models/MatchList.cs
@@ -1,6 +1,48 @@
-﻿namespace Tournament.Models
+﻿using System.Collections.Generic;
+
+namespace Tournament.Models
 {
-    class MatchList { }
+    class MatchesList
+    {
+        private List<Match> matchList;
+
+        /// <summary>
+        /// Initalizes a new instance of MatchList
+        /// </summary>
+        public MatchesList()
+        {
+            var matchList = new List<Match>();
+        }
+        /// <summary>
+        /// Gets a MatchList value (List<Match>)  
+        /// </summary>
+        public List<Match> GetMatchesList 
+        {
+            get => matchList;
+        }
+        /// <summary>
+        /// Adds Match to MatchList instance
+        /// </summary>
+        public void AddMatch(Match match) 
+        {
+            if (!GetMatchesList.Contains(match))
+                matchList.Add(match);
+        }
+        /// <summary>
+        /// Saves MatchList to specified file
+        /// </summary>
+        public void SaveMatchList(string path) 
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Match> LoadMatchesList(string path)
+        {
+            throw new System.NotImplementedException(); 
+           
+        }
+    
+    }
 
 
 
