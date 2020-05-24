d220453fcfa4c69415302206fad8df1dd06928ea
Osko <ia.kowalczyk5@gmail.com>
2020-05-25 00:08:23 +0200

    ID

--- Tournament/Models/League.cs
+++ Tournament/Models/League.cs
@@ -1,15 +1,16 @@
-﻿namespace Tournament.Models
+﻿using System.Windows.Documents;
+
+namespace Tournament.Models
 {
     class League {
 
 
         private MatchList matchesPlanned;
 
-        
+        public League(TeamList teams, RefereeList refrees)
+        {
+        }
 
 
     }
-
-    
-
 }
