396aef744688cffdf1051c77edf992c3771f6b07
neonv13 <neonv13@users.noreply.github.com>
2020-05-25 00:17:20 +0200

    Added some function to RefereeList

--- Tournament/Models/RefereeList.cs
+++ Tournament/Models/RefereeList.cs
@@ -3,10 +3,13 @@
 namespace Tournament.Models
 {
     using System.Collections.Generic;
+    using System.Threading;
+
     public class RefereeList
     {
 
         private List<Referee> referees;
+        private int count;
 
         /// <summary>
         /// Initializes a new istance of RefereesList
@@ -16,10 +19,17 @@ namespace Tournament.Models
         /// <summary>
         /// Adds Referee to RefereesList
         /// </summary>
+        
         public void AddReferee(Referee referee)
         {
             RefereesList.Add(referee);
         }
+
+        /// <summary>
+        /// Initializes a Count
+        /// </summary>
+        private int Count { get => count; set => count = value; }
+
         /// <summary>
         /// Retruns a instance of Player that has given ID
         /// </summary>
@@ -32,6 +42,37 @@ namespace Tournament.Models
             }
             return null;
         }
+
+        /// <summary>
+        /// Removes Referee from RefereesList by ID
+        /// </summary>
+        public void RemoveReferee(int id)
+        {
+            foreach (var referee in referees)
+            {
+                if (referee.ID == id)
+                {
+                    RefereesList.Remove(referee);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes Referee from RefereesList by Surname and Name
+        /// </summary>
+        public void RemoveReferee(string surName, string name)
+        {
+            foreach (var referee in referees)
+            {
+                if (referee.Surname == surName && referee.Name == name)
+                {
+                    RefereesList.Remove(referee);
+                }
+            }
+        }
+
+
+
 
     }
 
